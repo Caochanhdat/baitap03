3fb68079b932524b02850a3eb13822894af1a124
Caochanhdat <148928733+Caochanhdat@users.noreply.github.com>
2024-05-26 23:46:14 +0700

    Add files via upload

---
+++ lthdt/src/edu/pxu/lthdt/baitap03/XeBus.java
@@ -0,0 +1,63 @@
+package edu.pxu.lthdt.baitap03;
+
+public class XeBus {
+	    private String hangxe;
+	    private int namsanxuat;
+	    private float dongco;
+	    private int soghe;
+
+	    public XeBus(String hangxe, int namsanxuat, float dongco, int soghe) {
+	        this.hangxe = hangxe;
+	        this.namsanxuat = namsanxuat;
+	        this.dongco = dongco;
+	        this.soghe = soghe;
+	    }
+
+	    public String getHangxe() {
+	        return hangxe;
+	    }
+
+	    public void setHangxe(String hangxe) {
+	        this.hangxe = hangxe;
+	    }
+
+	    public int getNamsanxuat() {
+	        return namsanxuat;
+	    }
+
+	    public void setNamsanxuat(int namsanxuat) {
+	        this.namsanxuat = namsanxuat;
+	    }
+
+	    public float getDongco() {
+	        return dongco;
+	    }
+
+	    public void setDongco(float dongco) {
+	        this.dongco = dongco;
+	    }
+
+	    public int getSoghe() {
+	        return soghe;
+	    }
+
+	    public void setSoghe(int soghe) {
+	        this.soghe = soghe;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "XeBus{" +
+	                "hangxe='" + hangxe + '\'' +
+	                ", namsanxuat=" + namsanxuat +
+	                ", dongco=" + dongco +
+	                ", soghe=" + soghe +
+	                '}';
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
